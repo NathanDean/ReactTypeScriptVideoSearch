aa2a9676868d01504148b3b008469e22ca018612
NathanDean <nathandean@hotmail.co.uk>
2022-09-12 16:06:46 +0100

    Removed commented class components

--- src/components/VideoItem.tsx
+++ src/components/VideoItem.tsx
@@ -30,39 +30,4 @@ const VideoItem = ({video, onVideoSelect}) => {
 
 }
 
-// class VideoItem extends React.Component {
-    
-//     render(){
-
-//         const video = this.props.video;
-//         const onVideoSelect = this.props.onVideoSelect;
-
-//         return(
-
-//             <div
-//                 className = "video-item"
-//                 onClick = {() => onVideoSelect(video)}>
-                
-//                 <img 
-//                     src = {video.snippet.thumbnails.medium.url} 
-//                 />
-
-//                 <div>
-
-//                     <p className = "item-title">
-                    
-//                         {he.decode(video.snippet.title)}
-
-//                     </p>
-
-//                 </div>
-
-//             </div> 
-
-//         )
-
-//     }
-
-// }
-
 export default VideoItem
--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -21,27 +21,4 @@ const VideoList = ({videos, onVideoSelect}) => {
 
 }
 
-// class VideoList extends React.Component {
-    
-//     render(){
-
-//         const videos = this.props.videos;
-//         const onVideoSelect = this.props.onVideoSelect;
-
-//         const renderedList = videos.map(video => {
-//             return <VideoItem
-//                 key = {video.id.videoId}
-//                 video = {video} 
-//                 onVideoSelect = {onVideoSelect}
-//              />
-//         })
-
-//         return(
-//             <div className = "video-list">
-//                 {renderedList}
-//             </div>
-//         )
-//     }
-// }
-
 export default VideoList
